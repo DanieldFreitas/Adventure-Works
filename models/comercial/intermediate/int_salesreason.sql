b1b1c19d4ae354fd74149b7a143dae0850ef1e8b
Daniel de Freitas <danieldefreitas369@gmail.com>
2024-11-22 03:40:35 +0000

    finalizada novamente a dim salesreason

--- models/comercial/intermediate/int_salesreason.sql
+++ models/comercial/intermediate/int_salesreason.sql
@@ -2,21 +2,27 @@ with
     motivo_venda as (
         select * 
         from {{ ref('sgt_erp_salesreason') }}
-    )
-    , ordem_motivo_venda as (
+    ),
+    ordem_motivo_venda as (
         select *
         from {{ ref('sgt_erp_salesorderheadersalesreason') }}
-    )
-
-    , joneid as (
+    ),
+    joneid as (
         select 
-        motivo_venda.PK_MOTIVOVENDA
-          , motivo_venda.NOME_LOJA
-          , motivo_venda.TIPO_VENDA
-          , ordem_motivo_venda.FK_ORDEMVENDA
+            motivo_venda.PK_MOTIVOVENDA,
+            motivo_venda.TIPO_VENDA,
+            motivo_venda.NOME_LOJA,
+            ordem_motivo_venda.FK_ORDEMVENDA
         from motivo_venda
-        join ordem_motivo_venda on motivo_venda.pk_motivovenda = ordem_motivo_venda.fk_motivovenda
+        full outer join ordem_motivo_venda 
+        on motivo_venda.PK_MOTIVOVENDA = ordem_motivo_venda.FK_MOTIVOVENDA
     )
-
-select *
-from joneid 
+select 
+    TIPO_VENDA,
+    MIN(NOME_LOJA) as NOME_LOJA, -- Escolhe um nome de loja (menor valor alfabético)
+    LISTAGG(FK_ORDEMVENDA, ', ') WITHIN GROUP (ORDER BY FK_ORDEMVENDA) as FK_ORDEMVENDAS -- Concatena as FKs
+from joneid
+group by 
+    TIPO_VENDA
+order by 
+    TIPO_VENDA
